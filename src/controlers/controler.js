e94ada33848fbd5f23e9daf49b9b5c1c03a147f8
hoangphuc10x <phuc1092003@gmail.com>
2024-05-06 20:01:57 +0700

    income,vacation,shareholder,birthday

--- src/controlers/controler.js
+++ src/controlers/controler.js
@@ -1,18 +1,97 @@
-
 const { getEmployeeDataFromMySql, getEmployeeDataFromSqlServer,
+    getall_shareholder, getall_birthday,
 
-} = require('../services/service')
+} = require('../services/service');
 
-const getCombinedEmployeeData = async (req, res) => {
+const see_income = async (req, res) => {
     try {
-        // Fetch data from MySQL
         const mysqlData = await getEmployeeDataFromMySql();
         const sqlServerData = await getEmployeeDataFromSqlServer();
-        res.json({ mysqlData, sqlServerData });
+
+        // Map MySQL data to combine with SQL Server data
+        let combinedData = mysqlData.map(mysqlRow => {
+            // Find matching SQL Server row based on PERSONAL_ID
+            const matchingSqlServerRow = sqlServerData.find(sqlServerRow => sqlServerRow.PERSONAL_ID_PERSONAL === mysqlRow.idEmployee);
+
+            // Prepare combined object
+            const combinedObject = {
+                employee_id: mysqlRow.idEmployee,
+                personal_id: matchingSqlServerRow ? matchingSqlServerRow.PERSONAL_ID_PERSONAL : null,
+                first_name: mysqlRow.FirstName,
+                last_name: mysqlRow.LastName,
+                paid_to_date: mysqlRow.PaidToDate,
+                paid_last_year: mysqlRow.PaidLastYear,
+                value: mysqlRow.Value,
+                current_gender: matchingSqlServerRow ? matchingSqlServerRow.CURRENT_GENDER_PERSONAL : null,
+                department: matchingSqlServerRow ? matchingSqlServerRow.DEPARTMENT_JOB_HISTORY : null,
+                type_of_work: matchingSqlServerRow ? matchingSqlServerRow.TYPE_OF_WORK_DESCRIPTION : null,
+                ethnicity: matchingSqlServerRow ? matchingSqlServerRow.ETHNICITY_PERSONAL : null
+            };
+
+            return combinedObject;
+        });
+
+        // Sort the combined data by employee_id
+        combinedData.sort((a, b) => a.employee_id - b.employee_id);
+
+        // Send the sorted combined data as JSON
+        res.json(combinedData);
     } catch (error) {
         console.error('Error fetching combined employee data:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
 
-module.exports = { getCombinedEmployeeData };
+const see_vacationday = async (req, res) => {
+    try {
+        const mysqlData = await getEmployeeDataFromMySql();
+        const sqlServerData = await getEmployeeDataFromSqlServer();
+
+        // Map MySQL data to combine with SQL Server data
+        let combinedData = mysqlData.map(mysqlRow => {
+            // Find matching SQL Server row based on PERSONAL_ID
+            const matchingSqlServerRow = sqlServerData.find(sqlServerRow => sqlServerRow.PERSONAL_ID_PERSONAL === mysqlRow.idEmployee);
+
+            // Prepare combined object
+            const combinedObject = {
+                employee_id: mysqlRow.idEmployee,
+                personal_id: matchingSqlServerRow ? matchingSqlServerRow.PERSONAL_ID_PERSONAL : null,
+                first_name: mysqlRow.FirstName,
+                last_name: mysqlRow.LastName,
+                vacation_days: mysqlRow.VacationDays,
+                vacation_per_month: matchingSqlServerRow ? matchingSqlServerRow.TOTAL_NUMBER_VACATION_WORKING_DAYS_PER_MONTH_EMPLOYMENT_WORKING_TIME : null,
+                current_gender: matchingSqlServerRow ? matchingSqlServerRow.CURRENT_GENDER_PERSONAL : null,
+                department: matchingSqlServerRow ? matchingSqlServerRow.DEPARTMENT_JOB_HISTORY : null,
+                type_of_work: matchingSqlServerRow ? matchingSqlServerRow.TYPE_OF_WORK_DESCRIPTION : null,
+                ethnicity: matchingSqlServerRow ? matchingSqlServerRow.ETHNICITY_PERSONAL : null
+            };
+
+            return combinedObject;
+        });
+
+        // Sort the combined data by employee_id
+        combinedData.sort((a, b) => a.employee_id - b.employee_id);
+
+        // Send the sorted combined data as JSON
+        res.json(combinedData);
+    } catch (error) {
+        console.error('Error fetching combined employee data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+const see_avg_shareholder = async (req, res) => {
+    const data = await getall_shareholder();
+    return res.json({ data });
+};
+
+const see_birthday = async (req, res) => {
+    const data = await getall_birthday();
+    return res.json({ data });
+};
+
+
+module.exports = {
+    see_income, see_vacationday,
+    see_avg_shareholder, see_birthday
+};
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,42 +1,52 @@
 const express = require('express');
-const { gethomecontroler, gethomepage, createEmployee, create,
-    getEmployeeId, updateinfo, deleteEmployee, deleteinfo,
-    gethomepayrate,
-    //===========================================================
-    createpersonal, getbenefit, getemployment, getemployment_working, getjob_history, getpersonal,
-    getidpersonal,
-} = require('../controlers/homeControler');
-const { getCombinedEmployeeData } = require('../controlers/controler')
+// const { gethomecontroler, gethomepage, createEmployee, create,
+//     getEmployeeId, updateinfo, deleteEmployee, deleteinfo,
+//     gethomepayrate,
+//     //===========================================================
+//     createpersonal, getbenefit, getemployment, getemployment_working, getjob_history, getpersonal,
+// getidpersonal,} = require('../controlers/homeControler');
+const { see_income, see_vacationday,
+    see_avg_shareholder, see_birthday
+
+} = require('../controlers/controler')
 const router = express.Router();
 //-----------------------------------------------------------
-//employee
-router.get('/', gethomecontroler)
-router.get('/home', gethomepage)
-router.get('/create', create)
-// router.post('/getEmployeeId/:id', getEmployeeId)
-router.get('/create', create)
-router.post('/deleteEmployee/:id', deleteEmployee)
-router.post('/deleteinfo', deleteinfo)
+//employee{
+//     router.get('/', gethomecontroler)
+//     router.get('/home', gethomepage)
+//     router.get('/create', create)
+//     // router.post('/getEmployeeId/:id', getEmployeeId)
+//     router.get('/create', create)
+//     router.post('/deleteEmployee/:id', deleteEmployee)
+//     router.post('/deleteinfo', deleteinfo)
 
-//payrate
-router.get('/payrates', gethomepayrate)
+//     //payrate
+//     router.get('/payrates', gethomepayrate)
 
-//===========================================================
+//     //===========================================================
 
-router.get('/benefit', getbenefit)
-router.get('/employment', getemployment)
-router.get('/employment_working', getemployment_working)
-router.get('/job_history', getjob_history)
-router.get('/personal', getpersonal)
-router.post('/personal/:id', getidpersonal)
-
-
-// router.get('/personal', getpersonal)
-//create
-router.post('/createEmployee', createpersonal)
+//     router.get('/benefit', getbenefit)
+//     router.get('/employment', getemployment)
+//     router.get('/employment_working', getemployment_working)
+//     router.get('/job_history', getjob_history)
+//     router.get('/personal', getpersonal)
+//     router.post('/personal/:id', getidpersonal)
 
 
-//--------------------------------------
-router.get('/getinfo', getCombinedEmployeeData)
+//     // router.get('/personal', getpersonal)
+//     //create
+//     router.post('/createEmployee', createpersonal)
+
+
+//     //--------------------------------------
+// }
+
+router.get('/income', see_income)
+router.get('/vacation_days', see_vacationday)
+router.get('/shareholder', see_avg_shareholder)
+// inform
+router.get('/birthday', see_birthday)
+
+
 
 module.exports = router;
